a08de506e407c3678cd6efe5a1dce957a261f15d
hjzhhhj <jhj090120@gmail.com>
2025-02-11 05:44:52 +0900

    MAS 홈페이지 : 상단 바 구현

---
+++ src/page/MASwebsite.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./MASwebsite.css";
+
+const MASwebsite = () => {
+    return <div>
+        <div className="top-panel">
+            <p>MAS</p>
+        </div>
+    </div>
+}
+
+export default MASwebsite;
