6480608e6f7f0747efca4522755e798d824e932e
hjzhhhj <jhj090120@gmail.com>
2025-02-13 19:20:57 +0900

    MAS 홈페이지 : 4기 부원 추가

--- src/page/MASwebsite.tsx
+++ src/page/MASwebsite.tsx
@@ -1,32 +1,28 @@
 import React from "react";
 import "./MASwebsite.css";
 
-const members = [
-    {
-        name: "양은서", image: "", department: "sw",
-        hashtag: ["ENFP", "친화력 갑", "틱틱거리는 강아지", "공모전 천재", "MAS 찐러버", "귀여운 친칠라상"]
-    },
-    {
-        name: "피은서", image: "", department: "sw",
-        hashtag: []
-    },
-    {
-        name: "안민선", image: "", department: "sw",
-        hashtag: ["INFP", "잘 들어줌", "체육 부장", "귀여운거 좋아함"]
-    },
-    {
-        name: "이서현", image: "", department: "de",
-        hashtag: []
-    },
-    {
-        name: "이경민", image: "", department: "de",
-        hashtag: []
-    },
+const members1 = [
+    { name: "양은서", image: "", department: "sw", hashtag: ["ENFP", "친화력 갑", "틱틱거리는 강아지", "공모전 천재", "MAS 찐러버", "귀여운 친칠라상"] },
+    { name: "피은서", image: "", department: "sw", hashtag: [] },
+    { name: "안민선", image: "", department: "sw", hashtag: ["INFP", "잘 들어줌", "체육 부장", "귀여운거 좋아함"] },
+    { name: "이서현", image: "", department: "de", hashtag: [] },
+    { name: "이경민", image: "", department: "de", hashtag: [] },
+];
+
+const members2 = [
+    { name: "정희진", image: "", department: "sw", hashtag: [] },
+    { name: "정현윤", image: "", department: "sw", hashtag: [] },
+    { name: "박준원", image: "", department: "sw", hashtag: [] },
+    { name: "박홍준", image: "", department: "sw", hashtag: [] },
+    { name: "문지우", image: "", department: "de", hashtag: [] },
+    { name: "한정훈", image: "", department: "de", hashtag: [] },
 ];
 
 const MASwebsite = () => {
-    const swMembers = members.filter(member => member.department === "sw");
-    const deMembers = members.filter(member => member.department === "de");
+    const swMembers1 = members1.filter(member => member.department === "sw");
+    const deMembers1 = members1.filter(member => member.department === "de");
+    const swMembers2 = members2.filter(member => member.department === "sw");
+    const deMembers2 = members2.filter(member => member.department === "de");
 
     return (
         <div>
@@ -39,7 +35,9 @@ const MASwebsite = () => {
                     #친화력 #자유로운 분위기 #포트폴리오 #전공스터디<br />
                     #1:1 라인선배 #다양하고 많은 공모전 #족보
                 </div>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSclEn1FB5iNiRxFy8RUpR9KvtbWPmGUwocaKgZOINxYemb3UQ/viewform?usp=header"><button className="apply">지원하기</button></a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSclEn1FB5iNiRxFy8RUpR9KvtbWPmGUwocaKgZOINxYemb3UQ/viewform?usp=header">
+                    <button className="apply">지원하기</button>
+                </a>
                 <div className="info">
                     25.00.00 ~ 25.00.00
                     <br />
@@ -49,7 +47,7 @@ const MASwebsite = () => {
 
             <div className="title">3기 부원</div>
             <div className="members-sw">
-                {swMembers.map((member, index) => (
+                {swMembers1.map((member, index) => (
                     <div key={index} className="member-card green">
                         {member.image ? (
                             <img src={member.image} alt={member.name} className="profile-pic" />
@@ -66,7 +64,7 @@ const MASwebsite = () => {
                 ))}
             </div>
             <div className="members-de">
-                {deMembers.map((member, index) => (
+                {deMembers1.map((member, index) => (
                     <div key={index} className="member-card orange">
                         {member.image ? (
                             <img src={member.image} alt={member.name} className="profile-pic" />
@@ -84,8 +82,8 @@ const MASwebsite = () => {
             </div>
 
             <div className="title2">4기 부원</div>
-            <div className="members-sw">
-                {swMembers.map((member, index) => (
+            <div className="members2-sw">
+                {swMembers2.map((member, index) => (
                     <div key={index} className="member-card green">
                         {member.image ? (
                             <img src={member.image} alt={member.name} className="profile-pic" />
@@ -101,8 +99,8 @@ const MASwebsite = () => {
                     </div>
                 ))}
             </div>
-            <div className="members-de">
-                {deMembers.map((member, index) => (
+            <div className="members2-de">
+                {deMembers2.map((member, index) => (
                     <div key={index} className="member-card orange">
                         {member.image ? (
                             <img src={member.image} alt={member.name} className="profile-pic" />
@@ -121,6 +119,5 @@ const MASwebsite = () => {
         </div>
     );
 };
-
 
 export default MASwebsite;
