ed0feaf9d503f2e6642a49b36d0451c0905d3563
hjzhhhj <jhj090120@gmail.com>
2025-03-01 18:36:36 +0900

    MAS HOMEPAGE : PROFILE PAGE

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import MASwebsite from './page/MASwebsite';
+import Profile from './page/Profile';  // 대소문자 맞추기
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <MASwebsite />
+    <Profile />
   </React.StrictMode>
 );
